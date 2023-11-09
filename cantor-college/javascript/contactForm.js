8811c7c0b84d2410255752db792763ced9265609
Montgomery18 <116822359+Montgomery18@users.noreply.github.com>
2023-11-09 14:24:50 +0000

    Delete cantor-college directory

--- cantor-college/javascript/contactForm.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    let formSubmission = document.getElementById("ContactUsForm");
-    formSubmission.addEventListener("submit",function(ev){
-        ev.preventDefault();
-        window.location = "http://localhost/cantor-college/thank-you.php";
-    })
-}())
